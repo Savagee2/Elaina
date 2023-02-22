175ee97bb2a9f2afbcc7d1c319ecaa4ecba0a926
Lord Savage <107442697+Savagee2@users.noreply.github.com>
2023-02-22 02:24:27 +0200

    Update config.js

--- config.js
+++ config.js
@@ -32,20 +32,20 @@
 require("dotenv").config();
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "918101187835";   // You can Change this number //
+  gg = "263716564623";   // You can Change this number //
 }
 
 // -------------------------------------------------------------- //
 
 
 global.owner = gg.split(",");
-global.mongodb = process.env.MONGODB || "mongodb+srv://fantox:xrto71r@cluster0.dnlowts.mongodb.net/?retryWrites=true&w=majority";
+global.mongodb = process.env.MONGODB || "mongodb+srv://new-user_mabla:Tapiwanashi@cluster0.uudhv.mongodb.net/test";
 global.sessionId = process.env.SESSION_ID || "ok";
 global.prefa = process.env.PREFIX || "-";
 global.tenorApiKey =
   process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
-global.packname = process.env.PACKNAME || `Atlas MD`;
-global.author = process.env.AUTHOR || "by: Team Atlas";
+global.packname = process.env.PACKNAME || `Elaina-Savage`;
+global.author = process.env.AUTHOR || "by: Team Savage";
 global.port = process.env.PORT || "8000";
 
 module.exports = {
